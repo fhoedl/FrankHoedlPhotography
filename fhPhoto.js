d2af14cf4bb535d3cd2aa45069efcfcd44763849
Frank Hoedl <frankhoedl@mac.com>
2020-07-01 23:24:35 -0400

    partial update todataset

--- fhPhoto.js
+++ fhPhoto.js
@@ -5,24 +5,404 @@ let fhPhotoApp = {
 fhPhotoApp.slides = [
 	(slide1 = {
 		cat: `automotive`,
-		src: `url('./assets/image1.jpg')`,
-		alt: "Ford driving on logging road"
+		src: `url('./assets/Auto 01 FH-Subaru Birds.jpg')`,
+		alt: "Subaru at ice cream stand with birds above on the wires",
 	}),
 	(slide2 = {
-		cat: `landscape`,
-		src: `url('./assets/image2.jpg')`,
-		alt: "Subaru at ice cream stand with birds above on the wires"
+		cat: `automotive`,
+		src: `url('./assets/Auto 02 FH-Ford Wide Roadway Shot.jpg')`,
+		alt: "Ford driving on logging road",
 	}),
 	(slide3 = {
 		cat: `automotive`,
-		src: `url('./assets/image3.jpg')`,
-		alt: "Toyota truck off road in deep snow"
+		src: `url('./assets/Auto 03 FH-Toyota Tundra Snow Spray.jpg')`,
+		alt: "Toyota truck off road in deep snow",
 	}),
 	(slide4 = {
+		cat: `automotive`,
+		src: `url('./assets/Auto 04 FH-Subaru Running.jpg')`,
+		alt: "Subaru running shot",
+	}),
+	(slide5 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide6 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide7 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide8 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide9 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide10 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide11 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide12 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide13 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide14 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide15 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide16 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide17 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide18 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide19 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide20 = {
+		cat: `automotive`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide21 = {
+		cat: `lifestyle`,
+		src: `url('./assets/Life 01 FH-BC Hydro Bathroom flooded.jpg')`,
+		alt: "woman in Bathrobe with tub overflowing in flooded bathroom",
+	}),
+	(slide22 = {
+		cat: `lifestyle`,
+		src: `url('./assets/Life 02 FH-Gain Mechanic.jpg')`,
+		alt: "waiting area in auto repair shop",
+	}),
+	(slide23 = {
+		cat: `lifestyle`,
+		src: `url('./assets/Life 04 FH-BC Hydro TV & Office Spread.jpg')`,
+		alt: "two images showing energy leaking out of electrical devices",
+	}),
+	(slide24 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide25 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide26 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide27 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide28 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide29 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide30 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide31 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide32 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide33 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide34 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide35 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide36 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide37 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide38 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide39 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide40 = {
+		cat: `lifestyle`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide41 = {
 		cat: `landscape`,
-		src: `url('./assets/image3.jpg')`,
-		alt: "Toyota truck off road in deep snow"
-	})
+		src: `url('./assets/Land 01 FH-RaiseTheRoof Chair.jpg')`,
+		alt: "Chair sitting on curbside",
+	}),
+	(slide42 = {
+		cat: `landscape`,
+		src: `url('./assets/Land 02 FH-Personal Hamilton Walkway.jpg')`,
+		alt: "Overhead Image of walkway thru trees",
+	}),
+	(slide43 = {
+		cat: `landscape`,
+		src: `url('./assets/Land 04 FH-Crime Stoppers River & Car Trunk.jpg')`,
+		alt: "two images, of quotation marks set in scenes",
+	}),
+	(slide44 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide45 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide46 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide47 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide48 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide49 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide50 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide51 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide52 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide53 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide54 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide55 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide56 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide57 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide58 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide59 = {
+		cat: `landscape`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide60 = {
+		cat: `people`,
+		src: `url('./assets/People 01 FH-Personal Natasha Portrait.jpg')`,
+		alt: "portait of young girl, tight headshot",
+	}),
+	(slide61 = {
+		cat: `people`,
+		src: `url('./assets/People 02 FH-Mastercard Pool.jpg')`,
+		alt: "boy with mask and snorkel underwater",
+	}),
+	(slide62 = {
+		cat: `people`,
+		src: `url('./assets/People 04 FH-Kraft Tent.jpg')`,
+		alt: "two boys snacking in makeshift tent",
+	}),
+	(slide63 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide64 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide65 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide66 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide67 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide68 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide69 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide70 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide71 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide72 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide73 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide74 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide75 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide76 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide77 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide78 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide79 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
+	(slide80 = {
+		cat: `people`,
+		src: `url('')`,
+		alt: "",
+	}),
 ];
 
 
@@ -82,6 +462,15 @@ goToGallery.addEventListener('click', function(e){
 
 // GALLERY NAVIGATION Event
 // ARROWS & SPLIT SCREEN Combined, Navigating thru gallery
+document.addEventListener('keydown', (e) => {
+    console.log('key event:', e.code)
+    e.code === `ArrowLeft`
+		? fhPhotoApp.back()
+		: e.code === `ArrowRight`
+		? fhPhotoApp.forward()
+		: null;
+})
+
 window.addEventListener("mousedown", (e) => {
     let width = document.documentElement.clientWidth;
     let X = e.clientX;
@@ -128,6 +517,7 @@ fhPhotoApp.forward = () => {
     (current === slidesByCategory.length - 1)? current = -1 : current;
     fhPhotoApp.slideRight();
 };
+
 fhPhotoApp.slideRight = () => {
     current++;
     fhPhotoApp.displayImage();
