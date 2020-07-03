adf4f26ce3b0217bb085323f49c62f153cd09248
Frank Hoedl <frankhoedl@mac.com>
2020-07-03 01:26:42 -0400

    preloader v1

--- styles/sass/copy.js
+++ styles/sass/copy.js
@@ -1,5 +1,4 @@
-let fhPhotoApp = {   
-};
+let fhPhotoApp = {};
 
 // DATA - Reference Data Set
 fhPhotoApp.slides = [
@@ -404,7 +403,7 @@ fhPhotoApp.slides = [
 		cat: `people`,
 		src: `url('./assets/People20FH-CrimeStoppersHoodie.jpg')`,
 		alt: "",
-	})
+	}),
 ];
 
 fhPhotoApp.logoImages = [
@@ -447,39 +446,36 @@ fhPhotoApp.logoImages = [
 		cat: `logo`,
 		src: `url('./assets/Logo08_FH-MastercardFish.jpg')`,
 		alt: "",
-	})
+	}),
 ];
-
 
 // ARGUMENTS
 fhPhotoApp.slidesByCategory = fhPhotoApp.slides; // Duplicate for Category Sorting
 fhPhotoApp.current = 0; //image counter
-fhPhotoApp.category = '' //temporary
-fhPhotoApp.length = fhPhotoApp.slides.length; 
-
+fhPhotoApp.category = ""; //temporary
+fhPhotoApp.length = fhPhotoApp.slides.length;
 
 // SELECTORS
-fhPhotoApp.logo = document.querySelector('.logoClip');
-fhPhotoApp.sliderImages = document.querySelector('.slide');
+fhPhotoApp.logo = document.querySelector(".logoClip");
+fhPhotoApp.sliderImages = document.querySelector(".slide");
 fhPhotoApp.sliderPrevious = document.querySelector(".previousSlide");
 fhPhotoApp.sliderNext = document.querySelector(".nextSlide");
-fhPhotoApp.arrowLeft = document.querySelector('#arrowLeft');
-fhPhotoApp.arrowRight = document.querySelector('#arrowRight');
-fhPhotoApp.goToGallery = document.querySelector('.portfolioContainer');
+fhPhotoApp.arrowLeft = document.querySelector("#arrowLeft");
+fhPhotoApp.arrowRight = document.querySelector("#arrowRight");
+fhPhotoApp.goToGallery = document.querySelector(".portfolioContainer");
 fhPhotoApp.goToPortfolio = document.querySelector(`#slider`);
 
-
 // NameSpacing Destructured.
-let{
-    current, 
-    category, 
-    slides, 
+let {
+	current,
+	category,
+	slides,
 	slidesByCategory,
 	logoImages,
-    length 
+	length,
 } = fhPhotoApp;
 
-const{
+const {
 	logo,
 	sliderImages,
 	sliderNext,
@@ -493,37 +489,36 @@ const{
 // INITIAL Assignment of Image Values
 // Thru Logo
 
-
 logo.style.cssText = `  background-image: ${logoImages[0].src}; 
                         background-size: cover;
 						background-position: 70% 100%; 
 						background-repeat: no-repeat;
-						`;						
+						`;
 
 // EVENT LISTENERS
 // LOGO Event
 logo.addEventListener("click", () => {
-    console.log(`selected logo`);
+	console.log(`selected logo`);
 });
 
 // PORTFOLIOS SELECTED Event
-goToGallery.addEventListener('click', function(e){
-	category = e.target.childNodes[1].className; 
-	// category = e.target.childNodes[1].className; 
-    console.log(`category is: `,category);
-    fhPhotoApp.setCategory();
-})
+goToGallery.addEventListener("click", function (e) {
+	category = e.target.childNodes[1].className;
+	// category = e.target.childNodes[1].className;
+	console.log(`category is: `, category);
+	fhPhotoApp.setCategory();
+});
 
 // GALLERY NAVIGATION Event
 // ARROWS & SPLIT SCREEN Combined, Navigating thru gallery
-document.addEventListener('keydown', (e) => {
-    console.log('key event:', e.code)
-    e.code === `ArrowLeft`
+document.addEventListener("keydown", (e) => {
+	console.log("key event:", e.code);
+	e.code === `ArrowLeft`
 		? fhPhotoApp.back()
 		: e.code === `ArrowRight`
 		? fhPhotoApp.forward()
 		: null;
-})
+});
 
 // window.addEventListener("mousedown", (e) => {
 //     let width = document.documentElement.clientWidth;
@@ -533,69 +528,66 @@ document.addEventListener('keydown', (e) => {
 //     // Arrows may be redundant depending on other elements
 //     arrowRight.addEventListener = ('click',() => {
 //         window.removeEventListener(`mousedown`, addEventListener);
-//         fhPhotoApp.forward();    
+//         fhPhotoApp.forward();
 //     })
 //     arrowLeft.addEventListener = ('click', () => {
 //         window.removeEventListener(`mousedown`, addEventListener);
 //         fhPhotoApp.back();
 //     })
-        // ARROWS ONLY, Navigating thru gallery
-        arrowRight.addEventListener('click', () => fhPhotoApp.forward());   
-        arrowLeft.addEventListener('click', () => fhPhotoApp.back());
-
+// ARROWS ONLY, Navigating thru gallery
+arrowRight.addEventListener("click", () => fhPhotoApp.forward());
+arrowLeft.addEventListener("click", () => fhPhotoApp.back());
 
 // FUNCTIONS
 // Clears all images...
 fhPhotoApp.reset = () => {
-    for(let i=0; i<slides.length; i++) {         
-        sliderImages.style.display = 'none';
-}};
+	for (let i = 0; i < slides.length; i++) {
+		sliderImages.style.display = "none";
+	}
+};
 
 // Base Image in the collection
 fhPhotoApp.startSlide = () => {
-    fhPhotoApp.reset();
-    sliderImages.style.display = 'block' 
+	fhPhotoApp.reset();
+	sliderImages.style.display = "block";
 };
 
 // Selecting Portfolio Category
-fhPhotoApp.setCategory = function() {
+fhPhotoApp.setCategory = function () {
 	slidesByCategory = slides.filter((slide) => slide.cat === category);
 	length = slidesByCategory.length;
 	current = 0; // Reset the Gallery to the First Image[0]
 	fhPhotoApp.displayImage();
 	goToPortfolio.scrollIntoView();
-	fhPhotoApp.preloadImages();
 };
 
 // Navigation of Gallery
 fhPhotoApp.forward = () => {
-    (current === slidesByCategory.length - 1)? current = -1 : current;
-    fhPhotoApp.slideRight();
+	current === slidesByCategory.length - 1 ? (current = -1) : current;
+	fhPhotoApp.slideRight();
 };
 
 fhPhotoApp.slideRight = () => {
-    current++;
+	current++;
 	fhPhotoApp.displayImage();
-	fhPhotoApp.preloadImages();
 };
-        /*  NOTE: based on count INCREASING...
+/*  NOTE: based on count INCREASING...
             ONCE WE GET TO LAST IMAGE( the condition)
                 sets "current" to -1, 
             FUNCTION slideRight()... adds 1, 
                 so "current" value of -1 + 1 = 0
             SINCE 0 is the first image, ... we loop to beginning.
         */
-   
+
 fhPhotoApp.back = () => {
-    (current === 0) ? current = slidesByCategory.length : current;
-    fhPhotoApp.slideLeft(); 
+	current === 0 ? (current = slidesByCategory.length) : current;
+	fhPhotoApp.slideLeft();
 };
 fhPhotoApp.slideLeft = () => {
-    current--;
+	current--;
 	fhPhotoApp.displayImage();
-	fhPhotoApp.preloadImages();
 };
-        /*  NOTE: based on count DECREASING...
+/*  NOTE: based on count DECREASING...
             ONCE WE GET TO THE FIRST IMAGE (the condition)
                 sets "current" to length value (the number of images)
             FUNCTION slideLeft()... subtracts 1,
@@ -604,42 +596,39 @@ fhPhotoApp.slideLeft = () => {
 
 // Display Image in Gallery
 fhPhotoApp.displayImage = function () {
-    sliderImages.style.cssText = `   background-image : ${slidesByCategory[current].src}; `;
-    sliderImages.setAttribute(`alt`, slidesByCategory[current].alt);
-	// console.log(`current: `, current);
-	// console.log(`length is `, length);
-	// fhPhotoApp.preloadImages();
-
-	// sliderNext.style.cssText = ` background-image : ${
-	// 	slidesByCategory[current + 1].src
-	// 	}; `;
-	// sliderPrevious.style.cssText = ` background-image : ${
-	// 	slidesByCategory[current - 1].src
-	// 	}; `;
-};
-
-fhPhotoApp.preloadImages = function () {
+	sliderImages.style.cssText = `   background-image : ${slidesByCategory[current].src}; `;
+	sliderImages.setAttribute(`alt`, slidesByCategory[current].alt);
+	console.log(current);
 	sliderNext.style.cssText = ` background-image : ${
 		slidesByCategory[current + 1].src
-		}; `;
+	}; `;
 	sliderPrevious.style.cssText = ` background-image : ${
 		slidesByCategory[current - 1].src
-		}; `;
-}
+	}; `;
+};
 
+// fhPhotoApp.preloadImages = function () {
+// 	current === slidesByCategory.length - 1 ? (current = -1) : current;
+// 	console.log(current + 1);
+// 	sliderNext.style.cssText = `
+// 					background-image : ${slidesByCategory[current + 1].src};
+// 					`;
+// 	console.log(slidesByCategory[current + 1].src);
+// }
 
 // Initialize
-fhPhotoApp.init = () =>{
-    fhPhotoApp.slideRight();
-    fhPhotoApp.slideLeft();
-}
+fhPhotoApp.init = () => {
+	fhPhotoApp.slideRight();
+	fhPhotoApp.slideLeft();
+};
 // Document Ready
 let ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
+	if (document.readyState != "loading") callback();
+	else document.addEventListener("DOMContentLoaded", callback);
+};
 ready(() => {
-    console.log('Welcome to Frank Hoedl Photography, all images are copyright Frank Hoedl')
-    fhPhotoApp.init();
+	console.log(
+		"Welcome to Frank Hoedl Photography, all images are copyright Frank Hoedl"
+	);
+	fhPhotoApp.init();
 });
-
